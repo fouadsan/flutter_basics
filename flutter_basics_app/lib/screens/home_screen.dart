3a02ef4d1ebfd402992d3fca4a55aac3eb8968ae
fouadsan <f.benayad95@gmail.com>
2021-07-24 21:29:24 +0100

    Completed chapter5

--- flutter_basics_app/lib/screens/home_screen.dart
+++ flutter_basics_app/lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-import '../widgets/drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 class MyHomePage extends StatefulWidget {
   static const routeName = '/home';
@@ -10,12 +12,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var name = "Not Assigned";
-  TextEditingController nameController = TextEditingController();
+  var url = Uri.parse("https://jsonplaceholder.typicode.com/photos");
+  var data;
+  Client client = http.Client();
 
   @override
   void initState() {
     super.initState();
+    _fetchData();
+  }
+
+  _fetchData() async {
+    var request = await client.get(url);
+    data = json.decode(request.body);
+    setState(() {});
   }
 
   @override
@@ -33,73 +43,20 @@ class _MyHomePageState extends State<MyHomePage> {
           style: Theme.of(context).textTheme.headline1,
         ),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Container(
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    spreadRadius: 2,
-                    blurRadius: 7,
-                    offset: Offset(0, 3), // changes position of shadow
-                  ),
-                ],
-              ),
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Column(
-                    children: <Widget>[
-                      CircleAvatar(
-                        backgroundImage: AssetImage(
-                          'assets/images/example.jpg',
-                        ),
-                        radius: 100,
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      Text(
-                        name,
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: TextField(
-                          controller: nameController,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: "Enter Your Name",
-                            labelText: "Name",
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+      body: data != null
+          ? ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(data[index]['title']),
+                  subtitle: Text("ID: ${data[index]['id']}"),
+                  leading: Image.network(data[index]['url']),
+                );
+              },
+              itemCount: data.length,
+            )
+          : Center(
+              child: CircularProgressIndicator(),
             ),
-          ),
-        ),
-      ),
-      drawer: MyDrawer(name: name, nameController: nameController),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          name = nameController.text;
-          setState(() {});
-        },
-        backgroundColor: Theme.of(context).primaryColor,
-        child: Icon(
-          Icons.check,
-          color: Colors.white,
-        ),
-      ),
     );
   }
 }
